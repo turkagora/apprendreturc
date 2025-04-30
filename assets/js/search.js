b05671c5a32a2c1f49ec25ca641adc6756cc2149
turkagora <turkagora@outlook.com>
2025-04-30 18:02:27 +0200

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,42 +4,48 @@
     var vars = query.split("&");
     for (var i = 0; i < vars.length; i++) {
       var pair = vars[i].split("=");
-      if (decodeURIComponent(pair[0]) === variable) {
-        return decodeURIComponent(pair[1].replace(/\+/g, " "));
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, "%20"));
       }
     }
-    return null;
+  }
+
+  function displaySearchResults(results, store) {
+    var searchResults = document.getElementById("search-results");
+
+    if (results.length) {
+      var appendString = "";
+      for (var i = 0; i < results.length; i++) {
+        var ref = results[i].ref;
+        var item = store[ref];
+        appendString += `<li><a href="${item.url}"><h3>${item.title}</h3></a>`;
+        appendString += `<p>${item.content.substring(0, 160)}...</p></li>`;
+      }
+      searchResults.innerHTML = appendString;
+    } else {
+      searchResults.innerHTML = "<li>No results found</li>";
+    }
   }
 
   var searchTerm = getQueryVariable("query");
-  var searchResults = document.getElementById("search-results");
 
   if (searchTerm) {
     document.getElementById("search-box").value = searchTerm;
 
     var idx = lunr(function () {
-      this.ref("url");
-      this.field("title");
-      this.field("author");
-      this.field("category");
+      this.ref("id");
+      this.field("title", { boost: 10 });
       this.field("content");
-
       for (var key in window.store) {
-        this.add(window.store[key]);
+        this.add({
+          id: key,
+          title: window.store[key].title,
+          content: window.store[key].content,
+        });
       }
     });
 
     var results = idx.search(searchTerm);
-
-    if (results.length > 0) {
-      results.forEach(function (result) {
-        var item = window.store[result.ref];
-        var li = document.createElement("li");
-        li.innerHTML = `<a href="${item.url}">${item.title}</a>`;
-        searchResults.appendChild(li);
-      });
-    } else {
-      searchResults.innerHTML = "<li>Aucun résultat</li>";
-    }
+    displaySearchResults(results, window.store);
   }
 })();
